d885d0c598d66f9b842e4f05133ed3bd9bd027a0
Denis <denisdemainov@gmail.com>
2020-11-10 14:16:46 +0300

    lesson 2_20

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
 'use strict';
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log("Test");
+    }
 
-function first() {
-    // Do smth
-    setTimeout(function() {
-        console.log(1);
-    }, 500);
-}
+};
 
-function second() {
-    console.log(2);
-}
+options.makeTest();
 
-first();
-second();
+const {border, bg} = options.colors;
+console.log(border);
+// console.log(Object.keys(options).length);
 
-function learnJS(lang, callback) {
-    console.log(`I learn: ${lang}`);
-    callback();
-}
+// console.log(options["colors"]["bolder"]);
 
-learnJS('JS', function() {
-    console.log('I learn this lesson');
-});
+// // delete options.name;
+
+// // console.log(options);
+// let counter = 0;
+
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имет значение ${options[key][i]}`);
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имет значение ${options[key]}`);
+//         counter++;
+//     }
+// }
+
+// console.log(counter);
