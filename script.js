9814d94ebcbbbc5d8c5a2525742451968a4f66ce
Denis <denisdemainov@gmail.com>
2020-11-12 12:38:51 +0300

    lessot2_24

--- script.js
+++ script.js
@@ -6,4 +6,24 @@ let strObj = new String(str) ;
 // console.log("str - ", typeof(str));
 // console.log("strObj - ", typeof(strObj));
 
-console.dir([1, 2, 3]);
+// console.dir([1, 2, 3]);
+
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: function() {
+        console.log("Hello");
+    }
+};
+
+const jonh = Object.create(soldier);
+//  const jonh = {
+//     health: 100
+//  };
+
+//  jonh.__proto__ = soldier;
+
+// Object.setPrototypeOf(jonh, soldier);
+
+//  console.log("jonh", jonh.armor);
+jonh.sayHello();
