3a38e9f5ec0168cf42796470ab8758940c47a75f
Denis <denisdemainov@gmail.com>
2020-11-18 13:37:54 +0300

    lesson2_26

--- script.js
+++ script.js
@@ -1,29 +1,2 @@
 "use strict";
 
-let str = "some";
-let strObj = new String(str) ;
-
-// console.log("str - ", typeof(str));
-// console.log("strObj - ", typeof(strObj));
-
-// console.dir([1, 2, 3]);
-
-const soldier = {
-    health: 400,
-    armor: 100,
-    sayHello: function() {
-        console.log("Hello");
-    }
-};
-
-const jonh = Object.create(soldier);
-//  const jonh = {
-//     health: 100
-//  };
-
-//  jonh.__proto__ = soldier;
-
-// Object.setPrototypeOf(jonh, soldier);
-
-//  console.log("jonh", jonh.armor);
-jonh.sayHello();
